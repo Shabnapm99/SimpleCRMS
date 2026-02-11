c4d5cde7a3fb6caffaf4a00035bdc72c54b4e65f
Shabna P M <pmshabnashareefa@gmail.com>
2026-02-11 13:16:59 +0300

    Implemented DB connection

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import express from 'express';
+import { dbConnect } from '../config/db.js';
 
 const app = express();
 const PORT = 4000;
 
+dbConnect();//calling the function to connect to db
 app.listen(PORT,()=>console.log(`Server is running on port : ${PORT}`));
